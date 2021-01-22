938f87d2c2a3cbf4f44ce61d06e60f9d75ac0cab
ramadika <rzdka18@gmail.com>
2021-01-22 22:51:57 +0700

    login n signup

--- src/components/CreateAccount-Page/index.js
+++ src/components/CreateAccount-Page/index.js
@@ -1,6 +1,7 @@
 // Dependencies
 import React, { Component } from 'react'
 import { NavLink } from "react-router-dom";
+import { IoMdArrowRoundBack } from 'react-icons/io'
 // Internals
 import 'components/CreateAccount-Page/index.css'
 
@@ -8,25 +9,42 @@ export default class index extends Component {
     render() {
         return (
             <div className="container signUp">
-            <div className="row">
-                <h2>Create Account</h2>
-            </div>
-            <form className="form-signUp">
-                <label>Email</label>
-                <input type="email" value="" id="email" placeholder="Email" className="form-control text-center"></input>
-                <label>Username</label>
-                <input type="text" value="" id="username" placeholder="Username" className="form-control text-center"></input>
-                <label>Password</label>
-                <input type="password" value="" id="password" placeholder="Password" className="form-control text-center"></input>
-                <label>Name</label>
-                <input type="text" value="" id="name" placeholder="Name" className="form-control text-center"></input>
-                <label>Address</label>
-                <input type="text" value="" id="address" placeholder="Address" className="form-control text-center"></input>
-                <label>Phone Number</label>
-                <input type="text" value="" id="phoneNumber" placeholder="Phone Number" className="form-control text-center"></input>
-                {/* <button type="submit" className="btn">Create Account</button> */}
-                <button type="submit" className="btn"><NavLink to="/shop">Create Account</NavLink></button>
-            </form>
+                <div className="row">
+                    <h6>Arnawa</h6>
+                </div>
+                <div className="row">
+                    <h2>Create Account</h2>
+                </div>
+                <hr />
+                <form className="form-signUp mt-2">
+                    <div className="form-group row">
+                        <label className="col">Email</label>
+                        <input type="email" value="" id="email" className= "col form-control text-center"></input>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col">Username</label>
+                        <input type="text" value="" id="username" className= "col form-control text-center"></input>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col">Password</label>
+                        <input type="password" value="" id="password" className= "col form-control text-center"></input>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col">Name</label>
+                        <input type="text" value="" id="name" className= "col form-control text-center"></input>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col">Address</label>
+                        <input type="text" value="" id="address" className= "col form-control text-center"></input>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col">Phone Number</label>
+                        <input type="text" value="" id="phoneNumber" className= "col form-control text-center"></input>
+                    </div>
+                    {/* <button type="submit" className="btn">Create Account</button> */}
+                    <button type="submit" className="btn"><NavLink to="/" className="aCreate">Create Account</NavLink></button>
+                </form>
+                <NavLink to="/shop"><IoMdArrowRoundBack />Back to Shopping</NavLink>
             </div>
         )
     }
--- src/components/Login-Page/index.js
+++ src/components/Login-Page/index.js
@@ -9,13 +9,17 @@ export default class index extends Component {
         return (
             <div className="container login">
                 <div className="row">
+                    <h6>Arnawa</h6>
+                </div>
+                <div className="row">
                     <h2>Sign In</h2>
                 </div>
+                <hr />
                 <form className="form-login">
                     <input type="text" value="" id="username" placeholder="Username/Email" className="form-control text-center"></input>
                     <input type="password" value="" id="password" placeholder="Password" className="form-control text-center"></input>
-                    <button type="submit" className="btn">Sign In</button>
-                    {/* <button type="submit" className="btn"><NavLink to="/home">Sign In</NavLink></button> */}
+                    {/* <button type="submit" className="btn">Sign In</button> */}
+                    <button type="submit" className="btn"><NavLink to="/shop" className="aLogin">Sign In</NavLink></button>
                 </form>
                 <h4 className="mt-5 ml-5"> You are new?{" "} <NavLink to="/createAccount"><b>Create New</b></NavLink></h4>
             </div>
