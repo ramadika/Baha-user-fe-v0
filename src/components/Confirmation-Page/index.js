9d263f6c13c9939031062f6793a0d98735f5f766
ramadika <rzdka18@gmail.com>
2021-01-28 11:45:17 +0700

    confirm payment

--- src/components/Confirmation-Page/index.js
+++ src/components/Confirmation-Page/index.js
@@ -1,5 +1,8 @@
 // Dependencies
 import React, { Component } from 'react'
+import { NavLink } from "react-router-dom";
+import NavBar from 'components/Base-Layout/Nav-Bar'
+import Footer from 'components/Base-Layout/Footer'
 // Internals
 import 'components/Confirmation-Page/index.css'
 
@@ -7,7 +10,43 @@ export default class index extends Component {
     render() {
         return (
             <div>
-                
+                <NavBar />
+                <div className="container confirm">
+                    <h6 className="text-center">Arnawa</h6>
+                    <h2 className="text-center">Payment Confirmation</h2>
+                    <hr />
+                    <div className="row d-flex justify-content-center">
+                        <form className="form-confirm mt-2">
+                            <div className="form-group row">
+                                <label className="col">Order No</label>
+                                <input type="text" value="" id="orderNo" className= "col form-control text-center"></input>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col">Email</label>
+                                <input type="email" value="" id="email" className= "col form-control text-center"></input>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col">Destination Bank</label>
+                                <input type="text" value="" id="name" className= "col form-control text-center"></input>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col">Payment Amount</label>
+                                <select className="form-control">
+                                    <option selected value="bca">Bank BCA</option>
+                                    <option value="mandiri">Bank Mandiri</option>
+                                </select>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col">Upload File</label>
+                                <input type="file" value="" id="uploadConfrim" className="col"></input>
+                            </div>
+                            {/* <button type="submit" className="btn">Confirm Payment</button> */}
+                            <button type="submit" className="btn d-flex justify-content-center"><NavLink to="/login" className="aPayment">Confirm Payment</NavLink></button>
+                        </form>
+                    </div>
+                    <div className="row img-confirm"></div>
+                </div>
+                <Footer />
             </div>
         )
     }
