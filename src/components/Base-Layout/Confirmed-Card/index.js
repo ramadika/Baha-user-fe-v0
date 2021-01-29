381cd85e5aa8782f647360b32460d76b844b5733
ramadika <rzdka18@gmail.com>
2021-01-29 13:47:47 +0700

    confirmed

---
+++ src/components/Base-Layout/Confirmed-Card/index.js
@@ -0,0 +1,27 @@
+// Dependencies
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Footer from 'components/Base-Layout/Footer'
+import { TiTickOutline } from 'react-icons/ti'
+// Internals
+import 'components/Base-Layout/Confirmed-Card/index.css'
+
+export default function f() {
+    return (
+        <div>
+            <div className="container mb-5">
+                <div className="d-flex justify-content-center confirmed">
+                    <h2 className="text-center">
+                        <span><TiTickOutline size={150} className="tick-confirmed"/></span>
+                        <span className="brand-confirmed">Arnawa</span>
+                        <span>Thank you</span>
+                        <span className="message-confirmed">We are started on your order right away and you will receive an order confirmation email shortly to JohnNewman@gmail.com. 
+                            In the meantime, explore the latest fashion and get inspired by new trends, 
+                            just head over to <NavLink to="/shop">Arnawa</NavLink></span>
+                    </h2>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
