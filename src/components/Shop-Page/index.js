d718eefdeb9abaceb71de6ff43fded16ba40eb98
ramadika <rzdka18@gmail.com>
2021-02-01 15:18:46 +0700

    shop

--- src/components/Shop-Page/index.js
+++ src/components/Shop-Page/index.js
@@ -1,22 +1,47 @@
 // Dependencies
 import React, { Component } from 'react'
+import { NavLink } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import CardImg from "react-bootstrap/CardImg";
 import NavBar from 'components/Base-Layout/Nav-Bar'
 import Header from 'components/Base-Layout/Header'
-import Banner from 'components/Base-Layout/Banner'
 import Footer from 'components/Base-Layout/Footer'
 // Internals
 import 'components/Shop-Page/index.css'
+import ImgCard1 from "assets/images/artem-beliaikin-pPzQP35zh4o-unsplash.jpg";
+import ImgCard2 from "assets/images/markus-spiske-5UJbKYUjFCk-unsplash.jpg";
+import ImgCard3 from "assets/images/markus-spiske-zVdAkU4s9lI-unsplash.jpg";
 
 export default class index extends Component {
     render() {
         return (
             <div>
                 <NavBar />
-                <div className="container shop">
+                <div className="header-shop">
                     <Header />
-                    <Banner />
+                    <div className="d-flex justify-content-center">
+                        <NavLink to="/men" className="btn aCategory">Men</NavLink>
+                        <NavLink to="/women" className="btn aCategory">Women</NavLink>
+                        <NavLink to="/accessories" className="btn aCategory">Accessories</NavLink>
+                    </div>
+                </div>
+                <div className="container shop">
                     <div className="row d-flex justify-content-center mt-3">
-                        <h2>Shop</h2>
+                        <div className="col">
+                            <Card>
+                                <CardImg variant="top" src={ImgCard1} />
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card>
+                                <CardImg variant="top" src={ImgCard2} />
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card>
+                                <CardImg variant="top" src={ImgCard3} />
+                            </Card>
+                        </div>
                     </div>
                 </div>
                 <Footer />
