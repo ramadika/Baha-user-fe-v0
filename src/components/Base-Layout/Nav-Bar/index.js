46c0ed822603deb6003337ebcd7ac05ca65aa7da
ramadika <rzdka18@gmail.com>
2021-03-20 16:15:43 +0700

    navbar responsive

--- src/components/Base-Layout/Nav-Bar/index.js
+++ src/components/Base-Layout/Nav-Bar/index.js
@@ -9,30 +9,33 @@ import 'components/Base-Layout/Nav-Bar/index.css'
 export default function index() {
     return (
         <div>
-            <Navbar className="justify-content-center Navbar" bg="light" variant="light">
-                <Nav className="mr-5 Nav">
-                    <NavLink className="NavLink" to="/newArrival">
-                        NEW ARRIVAL
-                    </NavLink>
-                    <NavLink className="NavLink" to="/shop">
-                        SHOP
-                    </NavLink>
-                    <NavLink className="NavLink" to="/sale">
-                        SALE
-                    </NavLink>
-                    <NavLink className="NavLink" to="/customYou">
-                        CUSTOMIZE
-                    </NavLink>
-                    <NavLink className="NavLink" to="/Confirmation">
-                        CONFIRMATION
-                    </NavLink>
-                    <NavLink className="NavLink" to="/about">
-                        ABOUT
-                    </NavLink>
-                    <NavLink className="NavLink" to="/contact">
-                        CONTACT
-                    </NavLink>
-                </Nav>
+            <Navbar className="Navbar" bg="light" variant="light" expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav" >
+                    <Nav className="mr-5 Nav">
+                        <NavLink className="NavLink" to="/newArrival">
+                            NEW ARRIVAL
+                        </NavLink>
+                        <NavLink className="NavLink" to="/shop">
+                            SHOP
+                        </NavLink>
+                        <NavLink className="NavLink" to="/sale">
+                            SALE
+                        </NavLink>
+                        <NavLink className="NavLink" to="/customYou">
+                            CUSTOMIZE
+                        </NavLink>
+                        <NavLink className="NavLink" to="/Confirmation">
+                            CONFIRMATION
+                        </NavLink>
+                        <NavLink className="NavLink" to="/about">
+                            ABOUT
+                        </NavLink>
+                        <NavLink className="NavLink" to="/contact">
+                            CONTACT
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         </div>
     )
