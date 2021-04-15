20fa83c0b6bc368e6a872877e422954a1882ccdc
ramadika <rzdka18@gmail.com>
2021-04-15 09:38:18 +0700

    selected resp

--- src/components/Shop-Page/ProductSelected/index.js
+++ src/components/Shop-Page/ProductSelected/index.js
@@ -1,6 +1,7 @@
 // Dependencies
 import React, { Component } from 'react'
 import { NavLink } from "react-router-dom";
+import Header from 'components/Base-Layout/Header'
 import NavBar from 'components/Base-Layout/Nav-Bar'
 import Footer from 'components/Base-Layout/Footer'
 // Internals
@@ -33,6 +34,12 @@ export default class index extends Component {
         return (
             <div>
                 <NavBar />
+                    <Header />
+                    <div className="d-flex justify-content-center">
+                        <NavLink to="/men" className="btn a-Category">Men</NavLink>
+                        <NavLink to="/women" className="btn a-Category">Women</NavLink>
+                        <NavLink to="/accessories" className="btn a-Category">Accessories</NavLink>
+                    </div>
                 {
                     product.map(item =>(
                         <div className="details" key={item._id}>
